03020b435d3775f237cf48086e30c35c6d09443f
emreylc <yalcnnemre@gmail.com>
2023-07-04 00:53:03 +0300

    asd

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/", (req, res) => {
+  res.send({
+    msg: "merhaba",
+  });
+});
 
-module.exports = app;
+app.get("/test",(req,res)=>{
+    res.send("merhabaaa")
+})
+
+app.listen(8999,()=>{
+    console.log("server is running")
+})
