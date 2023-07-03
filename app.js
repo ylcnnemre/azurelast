00cd044bcd974880c0b0fef80eb08e525cbbbb95
emreylc <yalcnnemre@gmail.com>
2023-07-03 21:56:48 +0300

    test1

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 
 
 
-module.exports = app;
+app.get("/",(req,res)=>{
+
+    res.send("adamm oolllll")
+})
+
+
+app.get("/test",(req,res)=>{
+
+    res.json({
+        msg : "testini sikerimm"
+    })
+})
+
+app.listen(3500,()=>{
+    console.log("server is running")
+})
